0982b8c0245bb89bdbcdcc6e1172c9e4e919cff0
KEERTHI C R <149789812+chandrakeerthiCR@users.noreply.github.com>
2025-03-25 19:23:13 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,30 @@
+package org.tnsif.accenture.c2tc.basicsofjava;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		 byte byteMax=127;
+		 byte byteMin=-128;
+		 System.out.println("Min range of byte "+byteMin +"Max range og byte  "+byteMax);
+		 
+		short ShortMax=32767;
+		short ShortMin=-32767;
+		System.out.println("Max range of short" +ShortMax +" Min range of short" +ShortMin);
+		
+		int MaxInt=2147483647;
+		int MinInt=-2147483647;
+		System.out.println("Max range of int"+MaxInt + " Min range of int"+MinInt);
+		
+		float f=3234.141243278345f;
+		double d=3456.14123435463d;
+		System.out.println("f value of float datatype "+f+ "d value of doble"+d);
+		
+		boolean flag=false;
+		System.out.println("boolean value "+flag);
+		
+		char ch='n';
+		System.out.println("the char value is "+ ch);
+
+	}
+
+}
