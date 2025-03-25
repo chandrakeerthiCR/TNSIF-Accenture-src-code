86cbdca5bbb87fc21608353e957d544f6a25096f
KEERTHI C R <149789812+chandrakeerthiCR@users.noreply.github.com>
2025-03-25 19:39:03 +0530

    Create tree.java

---
+++ tree.java
@@ -0,0 +1,21 @@
+
+package org.tnsif.accenture.c2tc.collections;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		Set<Integer> set=new TreeSet<>();
+		set.add(45);
+		set.add(6);
+		set.add(7);
+	   
+	    
+	    System.out.println("TreeSet :"+set);
+		
+
+	}
+
+}
