f0c0e8e974ab0542cf567f0a9836132de0d6b9a8
KEERTHI C R <149789812+chandrakeerthiCR@users.noreply.github.com>
2025-03-25 19:25:43 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,20 @@
+
+package org.tnsif.accenture.c2tc.basicsofjava;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+	int num[]= {2,3,4,};
+  System.out.println(num[2]);
+		
+		int num[]=new int[4];
+	     num[0]=23;
+	     num[1]=45;
+	     num[2]=56;
+	     
+	     System.out.println(num[0]);
+	     System.out.println(num[1]);
+	     System.out.println(num[2]);
+	}
+
+}
