b8f351deb27785eee4e05a72e00c06ad2ed0a208
KEERTHI C R <149789812+chandrakeerthiCR@users.noreply.github.com>
2025-03-25 19:23:56 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,11 @@
+package org.tnsif.accenture.c2tc.basicsofjava;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1="Java Program";
+		System.out.println(s1);
+
+	}
+
+}
